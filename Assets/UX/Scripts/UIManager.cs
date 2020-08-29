caf5a131b2a040713293e9ff723c96558c4c1c96
Himanshu Gupta <thinktank.himanshu@gmail.com>
2020-08-29 16:44:00 +0530

    unity project

---
+++ Assets/UX/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] ARPlaneManager planeManager;
+    [SerializeField] ARUXAnimationManager animationManager;
+
+    bool foundPlane;
+    bool goalReached;
+
+    void OnEnable()
+    {
+        PlaceObjectsOnPlane.OnObjectPlaced += () => animationManager.FadeOffUI();
+        ARSession.stateChanged += ShowFindPlaneUI;
+    }
+
+    void ShowFindPlaneUI(ARSessionStateChangedEventArgs args)
+    {
+        if (args.state == ARSessionState.SessionTracking)
+        {
+            animationManager.ShowCrossPlatformFindAPlane();
+        }
+    }
+
+    void Update()
+    {
+        if (PlanesFound() && !foundPlane)
+        {
+            // Hide scan ground anim
+            animationManager.FadeOffUI();
+            foundPlane = true;
+        }
+
+        if (foundPlane && animationManager.fadeOffComplete && !goalReached)
+        {
+            // Show tap anim
+            animationManager.ShowTapToPlace();
+            goalReached = true;
+        }
+    }
+
+    bool PlanesFound()
+    {
+        return planeManager?.trackables.count > 0;
+    }
+}
+
